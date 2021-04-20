73fee9ebcc617cc529c62f29debc12fdf75a1eef
ashishjagrawal <akjagrawal24@gmail.com>
2021-04-20 18:40:57 +0530

    small change

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,9 @@ const { check, validationResult } = require("express-validator");
 const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 
 router.post("/signup", [
-    check("name", "name should contain atleast 3 character").isLength({ min:5 }),
+    check("name", "name should contain atleast 3 character").isLength({ min: 3 }),
     check("email", "email is required").isEmail(),
-    check("password", "password should contain atleast 3 char").isLength({min: 3})
+    check("password", "password should contain atleast 3 char").isLength({ min: 3 })
 ], signup);
 
 
@@ -16,12 +16,12 @@ router.post(
     "/signin",
     [
         check("email", "email is required").isEmail(),
-        check("password", "password field is required").isLength({min:3})
+        check("password", "password field is required").isLength({ min: 3 })
     ],
     signin
 );
 
-router.get("/signout", signout )
+router.get("/signout", signout)
 
 
 
